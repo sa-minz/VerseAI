7e41c10a945e4ae2b036a86149b0913f90bdca56
sa-minz <saviabey2004@gmail.com>
2026-09-16 21:53:38 +0530

    Prepare VerseAI for deployment

--- app.py
+++ app.py
@@ -76,6 +76,8 @@ Requirements:
             "error": str(e)
         }), 500
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
